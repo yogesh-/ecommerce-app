27715d051571753dbd9fe91b10ed8d064a8b221e
yogesh- <yogesh.gupta38@gmail.com>
2022-03-24 21:03:52 +0530

    changed class to className

--- src/components/categories/categories.jsx
+++ src/components/categories/categories.jsx
@@ -1,30 +1,38 @@
 export const Categories = () => {
   return (
-    <div class="shop-for">
-      <div class="shop-for-elements">
-        <h1 class="h1">🛍️SHOP FOR🛍️</h1>
-        <div class="shop-for-images flex-row">
-          <div class="img-container-one">
-            <img class="square-img-shop" src="/assets/men.jpg" alt="men" />
+    <div className="shop-for">
+      <div className="shop-for-elements">
+        <h1 className="h1">🛍️SHOP FOR🛍️</h1>
+        <div className="shop-for-images flex-row">
+          <div className="img-container-one">
+            <img className="square-img-shop" src="/assets/men.jpg" alt="men" />
             <a href="/html/product_listing.html">
-              <button class="btn">
-                <p class="h4">MEN's</p>
+              <button className="btn">
+                <p className="h4">MEN's</p>
               </button>
             </a>
           </div>
-          <div class="img-container-two">
-            <img class="square-img-shop" src="/assets/women.jpg" alt="women" />
+          <div className="img-container-two">
+            <img
+              className="square-img-shop"
+              src="/assets/women.jpg"
+              alt="women"
+            />
             <a href="/html/product_listing.html">
-              <button class="btn">
-                <p class="h4">WOMEN's</p>
+              <button className="btn">
+                <p className="h4">WOMEN's</p>
               </button>
             </a>
           </div>
-          <div class="img-container-three">
-            <img class="square-img-shop" src="/assets/kids.jpg" alt="kids" />
+          <div className="img-container-three">
+            <img
+              className="square-img-shop"
+              src="/assets/kids.jpg"
+              alt="kids"
+            />
             <a href="/html/product_listing.html">
-              <button class="btn">
-                <p class="h4">KID's</p>
+              <button className="btn">
+                <p className="h4">KID's</p>
               </button>
             </a>
           </div>
