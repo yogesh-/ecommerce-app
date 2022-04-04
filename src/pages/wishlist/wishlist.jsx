1d554240c3818625c0420a6414d4823dc9a65df8
yogesh- <yogesh.gupta38@gmail.com>
2022-04-04 20:35:52 +0530

    removed commented code

--- src/pages/wishlist/wishlist.jsx
+++ src/pages/wishlist/wishlist.jsx
@@ -9,7 +9,6 @@ export const Wishlist = () => {
   const { wishState, wishDispatch } = useWish();
   const { cartDispatch } = useCart();
   const finalWish = wishState.wishProducts;
-  console.log(finalWish);
 
   if (finalWish.length === 0) {
     return (
@@ -81,51 +80,4 @@ export const Wishlist = () => {
       </div>
     );
   }
-
-  // return (
-  //   <div>
-  //     <Navbar />
-  //     {finalWish.map((item, index) => {
-  //       return (
-  //         <li key={index}>
-  //           <div className="card-badge">
-  //             <img src={item.image} alt="sneakers" />
-  //             <div className="card-tag flex-row">
-  //               <span className="material-icons-outlined">
-  //                 <img src="/assets/heart.png" alt="fav_icon" />
-  //               </span>
-  //             </div>
-
-  //             <div className="card-text">
-  //               <p className="h5">{item.brand}</p>
-  //               <p className="h2">{item.title}</p>
-  //               <p className="h3">
-  //                 {item.price} <span>{item.original_price}</span>
-  //               </p>
-  //               <div className="prod-buttons">
-  //                 <button className="btn">
-  //                   <p className="h4">Add to Cart</p>
-  //                 </button>
-
-  //                 <button
-  //                   className="btn btn-secondary"
-  //                   onClick={(e) =>
-  //                     wishDispatch({
-  //                       type: "REMOVE_FROM_WISHLIST",
-  //                       payload: item,
-  //                     })
-  //                   }
-  //                 >
-  //                   <p className="h4">Remove from Wishlist</p>
-  //                 </button>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </li>
-  //       );
-  //     })}
-
-  //     <Footer />
-  //   </div>
-  // );
 };
